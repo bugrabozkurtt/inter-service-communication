6f0b93dffb9f95894e1c94f0d4f65d61f840da9d
abfbilisim <bugrabozkurtt@gmail.com>
2024-11-25 20:26:43 +0300

    minimum stability set to dev v5

--- src/Config/inter_service_communication.php
+++ src/Config/inter_service_communication.php
@@ -1,1 +1,9 @@
 <?php
+
+return [
+    'base_uri' => env('SERVICE_BASE_URI', 'http://localhost'),
+    'timeout' => env('SERVICE_TIMEOUT', 5),
+    'headers' => [
+        'Accept' => 'application/json',
+    ],
+];
