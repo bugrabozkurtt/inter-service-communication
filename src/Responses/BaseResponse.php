d8f64981c1897856940bef151793acb0d1b4afcd
abfbilisim <bugrabozkurtt@gmail.com>
2024-11-29 02:20:55 +0300

    BaseResponse changed for boolean status and add data object when set data

--- src/Responses/BaseResponse.php
+++ src/Responses/BaseResponse.php
@@ -7,22 +7,37 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BaseResponse extends JsonResource
 {
-    public static function success($data = [], $message = 'Success', $code = 200): JsonResponse
+    public static function success($data = [], $message = null, $code = 200): JsonResponse
     {
-        return response()->json([
-            'status' => 'success',
-            'message' => $message,
-            'data' => $data
-        ], $code);
+        $response = [
+            'success' => true,
+        ];
+
+        if (!empty($message)) {
+            $response['message'] = $message;
+        }
+
+        if (!empty($data)) {
+            $response['data'] = $data;
+        }
+
+        return response()->json($response, $code);
     }
 
-    public static function error($message = 'Error', $code = 400, $data = []): JsonResponse
+    public static function error($message = null, $code = 400, $data = []): JsonResponse
     {
-        return response()->json([
-            'status' => 'error',
-            'message' => $message,
-            'data' => $data
-        ], $code);
-    }
+        $response = [
+            'success' => false,
+        ];
 
+        if (!empty($message)) {
+            $response['message'] = $message;
+        }
+
+        if (!empty($data)) {
+            $response['data'] = $data;
+        }
+
+        return response()->json($response, $code);
+    }
 }
